f20ea07fd9507c022858a52ca767ceb94f0a7e9f
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-07-26 15:38:41 +0530

    added test cases.

--- test/test.js
+++ test/test.js
@@ -2,3 +2,7 @@ var lc = require("../index");
 console.log(lc.AND(2, 3));
 console.log(lc.OR(2, 3));
 console.log(lc.Conversion(3, 2));
+
+console.log(lc.AND(8, 3));
+console.log(lc.OR(8, 3));
+console.log(lc.Conversion(16, 8));
