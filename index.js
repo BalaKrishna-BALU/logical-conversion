f2624dcffbeca15e5d089d0f943e1594fd118db2
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-07-27 23:53:01 +0530

    Added package.json file and added negation functionality

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ module.exports = {
 	OR : function(a, b) {
 		return ((a | b));
 	},
+	NOT:function(a){
+		var temp1 = (a >>> 0).toString(2);
+		var temp2='';
+		for(var i=0;i<temp1.length;i++){
+      		temp2+=parseInt(temp1[i])==1?0:1;
+		}
+		return parseInt(temp2);
+	},
 	Conversion : function(number, toRadix) {
 		return (number >>> 0).toString(toRadix);
 	}
--- test/test.js
+++ test/test.js
@@ -2,7 +2,9 @@ var lc = require("../index");
 console.log(lc.AND(2, 3));
 console.log(lc.OR(2, 3));
 console.log(lc.Conversion(3, 2));
+console.log(lc.NOT(8));
 
 console.log(lc.AND(8, 3));
 console.log(lc.OR(8, 3));
 console.log(lc.Conversion(16, 8));
+console.log(lc.NOT(5));
