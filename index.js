6d61cd01ade3f2aa9a762f95797a75af0f6b31c4
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-07-29 01:59:16 +0530

    Added XOR functionality.

--- index.js
+++ index.js
@@ -13,6 +13,36 @@ module.exports = {
 		}
 		return parseInt(temp2);
 	},
+	XOR:function(a,b){
+		var c = (a >>> 0).toString(2);
+		var d = (b >>> 0).toString(2);
+        var i=0,result='',temp='',minlen=0;
+        if(c.length>d.length){
+            minlen = d.length;
+            for(i=1;i<=minlen;i++){
+                result=(c[c.length-i]^d[d.length-i]).toString()+result;
+            }
+            c=c.split('');
+            for(var j=0;j<c.length-minlen;j++){
+                temp=temp+c[j];
+            }
+        }else if(c.length<d.length){
+            minlen = c.length;
+            for(i=1;i<=minlen;i++){
+                result=(c[c.length-i]^d[d.length-i]).toString()+result;
+            }
+            d=d.split('');
+            for(var j=0;j<d.length-minlen;j++){
+                temp=temp+d[j];
+            }
+        }else{
+            minlen = c.length;
+            for(i=1;i<=minlen;i++){
+                result=(c[c.length-i]^d[d.length-i]).toString()+result;
+            }
+        }
+        return temp+result;
+	},
 	Conversion : function(number, toRadix) {
 		return (number >>> 0).toString(toRadix);
 	}
--- test/test.js
+++ test/test.js
@@ -3,8 +3,11 @@ console.log(lc.AND(2, 3));
 console.log(lc.OR(2, 3));
 console.log(lc.Conversion(3, 2));
 console.log(lc.NOT(8));
+console.log("XOR",lc.XOR(9,4));
+console.log("XOR",lc.XOR(4,4));
 
 console.log(lc.AND(8, 3));
 console.log(lc.OR(8, 3));
 console.log(lc.Conversion(16, 8));
 console.log(lc.NOT(5));
+console.log("XOR",lc.XOR(7,16));
