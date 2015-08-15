74de280b55fb7c19b847b0eecc27d5b3d48b75da
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-08-16 01:49:35 +0530

    Updated index.js file with XNOR operation.

--- index.js
+++ index.js
@@ -9,37 +9,50 @@ module.exports = {
 		var temp1 = (a >>> 0).toString(2);
 		var temp2='';
 		for(var i=0;i<temp1.length;i++){
-      		temp2+=parseInt(temp1[i])==1?0:1;
+			temp2+=parseInt(temp1[i])==1?0:1;
 		}
 		return parseInt(temp2,2);
 	},
 	XOR:function(a,b){
 		var c = (a).toString(2);
 		var d = (b).toString(2);
-        var i=0,result='',temp='',minlen=0;
-        
-        if(c.length>d.length){
-            minlen = d.length;
-            performXOR(c.split(''));
-        }else if(c.length<d.length){
-            minlen = c.length;
-            performXOR(d.split(''));
-        }else{
-            minlen = c.length;
-            performXOR();
-        }
+		var i=0,result='',temp='',minlen=0;
 
-        function performXOR(splitnumber){
-             for(i=1;i<=minlen;i++){
-                result=(c[c.length-i]^d[d.length-i]).toString()+result;
-            }
-            if(splitnumber){
-                for(i=0;i<splitnumber.length-minlen;i++){
-                    temp=temp+splitnumber[i];
-                }
-            }
-        }
-        return parseInt((temp+result),2);
+		if(c.length>d.length){
+			minlen = d.length;
+			performXOR(c.split(''));
+		}else if(c.length<d.length){
+			minlen = c.length;
+			performXOR(d.split(''));
+		}else{
+			minlen = c.length;
+			performXOR();
+		}
+
+		function performXOR(splitnumber){
+			for(i=1;i<=minlen;i++){
+				result=(c[c.length-i]^d[d.length-i]).toString()+result;
+			}
+			if(splitnumber){
+				for(i=0;i<splitnumber.length-minlen;i++){
+					temp=temp+splitnumber[i];
+				}
+			}
+		}
+		return parseInt((temp+result),2);
+	},
+	XNOR:function(a,b){
+		var temp = this.XOR(a,b);
+		if(a===b){
+			var result='';
+			for(var i=1;i<((a).toString(2)).length;i++){
+				result+=1;
+			}
+			return parseInt((this.NOT(temp)+''+result),2);
+		}else{
+			return this.NOT(temp);
+		}
+		
 	},
 	Conversion : function(number, toRadix) {
 		return (number >>> 0).toString(toRadix);
