8977d9883280c79b8eb325792983b42a8e955129
Bala Krishna <balakrishnasadu.89@gmail.com>
2015-08-16 01:58:37 +0530

    Added test cases for XNOR.

--- test/test.js
+++ test/test.js
@@ -4,16 +4,19 @@ console.log("AND",lc.AND(2, 3));
 console.log("OR",lc.OR(2, 3));
 console.log("NOT",lc.NOT(8));
 console.log("XOR",lc.XOR(9,4));
+console.log("XNOR",lc.XNOR(9,4));
 console.log("Conversion",lc.Conversion(15, 2));
 
 console.log("AND",lc.AND(0, 7));
 console.log("OR",lc.OR(7, 0));
 console.log("NOT",lc.NOT(7));
-console.log("XOR",lc.XOR(4,4));
+console.log("XOR",lc.XOR(8,8));
+console.log("XNOR",lc.XNOR(8,8));
 console.log("Conversion",lc.Conversion(15, 8));
 
 console.log("AND",lc.AND(5, 9));
 console.log("OR",lc.OR(9, 5));
 console.log("NOT",lc.NOT(16));
 console.log("XOR",lc.XOR(7,16));
+console.log("XNOR",lc.XNOR(7,16));
 console.log("Conversion",lc.Conversion(7, 2));
