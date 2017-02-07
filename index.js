0563ddcec52e07f44ef56c97f756b9d862baad1e
test1 <test1.pws@gmail.com>
2017-02-07 19:37:47 +0530

    Added support for web applications as plugin usage

--- index.js
+++ index.js
@@ -1,59 +1,84 @@
-module.exports = {
-	AND : function(a, b) {
+(function () {
+	'use strict';
+
+	function AND(a, b) {
 		return ((a & b));
-	},
-	OR : function(a, b) {
+	}
+
+	function OR(a, b) {
 		return ((a | b));
-	},
-	NOT:function(a){
+	}
+
+	function NOT(a) {
 		var temp1 = (a >>> 0).toString(2);
-		var temp2='';
-		for(var i=0;i<temp1.length;i++){
-			temp2+=parseInt(temp1[i])==1?0:1;
+		var temp2 = '';
+		for (var i = 0; i < temp1.length; i++) {
+			temp2 += parseInt(temp1[i]) == 1 ? 0 : 1;
 		}
-		return parseInt(temp2,2);
-	},
-	XOR:function(a,b){
+		return parseInt(temp2, 2);
+	}
+
+	function XOR(a, b) {
 		var c = (a).toString(2);
 		var d = (b).toString(2);
-		var i=0,result='',temp='',minlen=0;
-
-		if(c.length>d.length){
+		var i = 0,
+			result = '',
+			temp = '',
+			minlen = 0;
+		if (c.length > d.length) {
 			minlen = d.length;
 			performXOR(c.split(''));
-		}else if(c.length<d.length){
+		} else if (c.length < d.length) {
 			minlen = c.length;
 			performXOR(d.split(''));
-		}else{
+		} else {
 			minlen = c.length;
 			performXOR();
 		}
 
-		function performXOR(splitnumber){
-			for(i=1;i<=minlen;i++){
-				result=(c[c.length-i]^d[d.length-i]).toString()+result;
+		function performXOR(splitnumber) {
+			for (i = 1; i <= minlen; i++) {
+				result = (c[c.length - i] ^ d[d.length - i]).toString() + result;
 			}
-			if(splitnumber){
-				for(i=0;i<splitnumber.length-minlen;i++){
-					temp=temp+splitnumber[i];
+			if (splitnumber) {
+				for (i = 0; i < splitnumber.length - minlen; i++) {
+					temp = temp + splitnumber[i];
 				}
 			}
 		}
-		return parseInt((temp+result),2);
-	},
-	XNOR:function(a,b){
-		var temp = this.XOR(a,b);
-		if(a===b){
-			var result='';
-			for(var i=1;i<((a).toString(2)).length;i++){
-				result+=1;
+		return parseInt((temp + result), 2);
+	}
+
+	function XNOR(a, b) {
+		var temp = this.XOR(a, b);
+		if (a === b) {
+			var result = '';
+			for (var i = 1; i < ((a).toString(2)).length; i++) {
+				result += 1;
 			}
-			return parseInt((this.NOT(temp)+''+result),2);
-		}else{
+			return parseInt((this.NOT(temp) + '' + result), 2);
+		} else {
 			return this.NOT(temp);
 		}
-	},
-	Conversion : function(number, toRadix) {
+	}
+
+	function Conversion(number, toRadix) {
 		return (number >>> 0).toString(toRadix);
 	}
-}
+	if (typeof exports !== "undefined" && typeof module !== "undefined" && module.exports) {
+		exports = module.exports = {};
+		exports.lcAND = AND;
+		exports.lcOR = OR;
+		exports.lcNOT = NOT;
+		exports.lcXOR = XOR;
+		exports.lcXNOR = XNOR;
+		exports.lcConversion = Conversion;
+	} else {
+		this.lcAND = AND;
+		this.lcOR = OR;
+		this.lcNOT = NOT;
+		this.lcXOR = XOR;
+		this.lcXNOR = XNOR;
+		this.lcConversion = Conversion;
+	}
+}).call(this);
